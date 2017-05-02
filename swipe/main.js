e927d37cb722428670b9c2f01b599ea76ea3012b
Atul Acharya <atul.acharya@gmail.com>
2017-05-01 22:39:04 -0700

    init commit: Swipe project

---
+++ swipe/main.js
@@ -0,0 +1,25 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+// local 
+import Ball from './src/Ball';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Ball />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+    
+  },
+});
+
+Expo.registerRootComponent(App);
---
+++ swipe/src/Ball.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { View, Animated } from 'react-native';
+
+
+class Ball extends Component {
+    // one time
+    componentWillMount() {
+        // init position
+        this.position = new Animated.ValueXY(0, 0);  // current position
+        // final position, with animation
+        Animated.spring(this.position, {
+            toValue: { x: 200, y: 500 }             // new position
+        }).start();                                 // default: x seconds
+    }
+
+    // main render
+    render() {
+        // NOTE: Animated.View is rendered exactly ONCE - since the Animation component runs separately from regular React!!
+        // Actual element being animated on UI is 'Animated.View', not the Ball per se
+        return (
+            <Animated.View style={this.position.getLayout()}>
+                <View style={styles.ball} />
+            </Animated.View>
+        );
+    }
+}
+
+// styling
+const styles = {
+    ball: {
+        height: 60,
+        width: 60,
+        borderRadius: 30,
+        borderWidth: 30,
+        borderColor: 'black'
+    }
+};
+
+// export
+export default Ball;
