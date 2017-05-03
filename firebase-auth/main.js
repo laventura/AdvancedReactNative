31f9187a5bd34874855bbd0bd2d001e61c0bf919
Atul Acharya <atul.acharya@gmail.com>
2017-05-03 01:18:55 -0700

    Added: Firebase authentication using Twilio/SMS done

--- firebase-auth/main.js
+++ firebase-auth/main.js
@@ -1,14 +1,34 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 // local imports
 import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
 
 class App extends React.Component {
+  // lifecycle
+  componentDidMount() {
+
+      // Initialize Firebase
+      const config = {
+        apiKey: "AIzaSyA9lScpuWdGm0_PARRxb_HUzb9X6MJRGkU",
+        authDomain: "one-time-password-106aa.firebaseapp.com",
+        databaseURL: "https://one-time-password-106aa.firebaseio.com",
+        projectId: "one-time-password-106aa",
+        storageBucket: "one-time-password-106aa.appspot.com",
+        messagingSenderId: "636840219839"
+      };
+      firebase.initializeApp(config);
+
+  }
+
+  // main render ------
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -19,7 +39,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
 
