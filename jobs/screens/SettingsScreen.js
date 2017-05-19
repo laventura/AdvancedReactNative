2f89c69568f9495e70abda1ddc223b667a7093a1
Atul Acharya <atul.acharya@gmail.com>
2017-05-18 23:48:18 -0700

    Done: Delete Liked Jobs ability added

--- jobs/screens/SettingsScreen.js
+++ jobs/screens/SettingsScreen.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import * as actions from '../actions';
 
 class SettingsScreen extends Component {
     render() {
+        // clear out any Liked Jobs
         return (
             <View>
-                <Text>SettingsScreen</Text>
-                <Text>SettingsScreen</Text>
-                <Text>SettingsScreen</Text>
-                <Text>SettingsScreen</Text>
+               <Button 
+                  title="Reset Liked Jobs"
+                  large
+                  icon={{ name: 'delete-forever' }}
+                  backgroundColor="#F44336"
+                  onPress={this.props.clearLikedJobs}
+               />
             </View>
         );
     }
 }
 
-export default SettingsScreen;
+// wire up Redux
+export default connect(null, actions)(SettingsScreen);
